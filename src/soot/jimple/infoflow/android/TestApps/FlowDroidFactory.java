7958a34f5fea331645026526480a44f76ff61b5c
William Snavely <wsnavely@cert.org>
2016-12-22 12:03:07 -0500

    Updating with new flowdroid options

--- src/soot/jimple/infoflow/android/TestApps/FlowDroidFactory.java
+++ src/soot/jimple/infoflow/android/TestApps/FlowDroidFactory.java
@@ -6,15 +6,21 @@ import java.util.Map;
 
 import org.xmlpull.v1.XmlPullParserException;
 
-import soot.jimple.infoflow.IInfoflow.CallgraphAlgorithm;
+import com.json.parsers.JSONParser;
+import com.json.parsers.JsonParserFactory;
+
+import soot.jimple.infoflow.Infoflow;
+import soot.jimple.infoflow.InfoflowConfiguration;
+import soot.jimple.infoflow.InfoflowConfiguration.AliasingAlgorithm;
+import soot.jimple.infoflow.InfoflowConfiguration.CallgraphAlgorithm;
+import soot.jimple.infoflow.InfoflowConfiguration.CodeEliminationMode;
+import soot.jimple.infoflow.android.InfoflowAndroidConfiguration;
+import soot.jimple.infoflow.android.InfoflowAndroidConfiguration.CallbackAnalyzer;
 import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.infoflow.android.source.AndroidSourceSinkManager.LayoutMatchingMode;
 import soot.jimple.infoflow.data.pathBuilders.DefaultPathBuilderFactory.PathBuilder;
 import soot.jimple.infoflow.ipc.IIPCManager;
 import soot.jimple.infoflow.taintWrappers.EasyTaintWrapper;
-
-import com.json.parsers.JSONParser;
-import com.json.parsers.JsonParserFactory;
 
 public class FlowDroidFactory {
 	abstract class Option<V> {
@@ -79,6 +85,10 @@ public class FlowDroidFactory {
 			super(def);
 			this.type = t;
 		}
+		
+		public EnumOption(Enum e) {
+			this(e.getClass(), e.toString());
+		}
 
 		@Override
 		public Enum getValue() {
@@ -88,27 +98,38 @@ public class FlowDroidFactory {
 
 	public Map<String, Option> getDefaultOptions() {
 		Map<String, Option> options = new HashMap<String, Option>();
-		options.put("flowSensitiveAliasing", new BooleanOption(false));
-		options.put("enableExceptions", new BooleanOption(false));
-		options.put("stopAfterFirstFlow", new BooleanOption(false));
-		options.put("computeResultPaths", new BooleanOption(false));
-		options.put("enableCallbacks", new BooleanOption(false));
-		options.put("staticTracking", new BooleanOption(false));
-		options.put("implicitFlows", new BooleanOption(false));
-		options.put("flowSensitiveAliasing", new BooleanOption(false));
-		options.put("aggressiveTaintWrapper", new BooleanOption(false));
-		options.put("pathBuilder", new EnumOption(PathBuilder.class,
-				"ContextInsensitiveSourceFinder"));
-		options.put("callgraphAlgorithm", new EnumOption(
-				CallgraphAlgorithm.class, "AutomaticSelection"));
-		options.put("layoutMatchingMode", new EnumOption(
-				LayoutMatchingMode.class, "MatchSensitiveOnly"));
-		options.put("sourcesAndSinksFile", new SimpleOption(
-				"SourcesAndSinks.txt"));
-		options.put("taintWrapperFile", new SimpleOption(
-				"EasyTaintWrapperSource.txt"));
-		options.put("androidPlatformsFolder", new SimpleOption(""));
 		options.put("accessPathLength", new IntOption(5));
+		options.put("aliasingAlgorithm", new EnumOption(AliasingAlgorithm.FlowSensitive));
+		options.put("callbackAnalyzer", new EnumOption(CallbackAnalyzer.Default));
+		options.put("callgraphAlgorithm", new EnumOption(CallgraphAlgorithm.AutomaticSelection));
+		options.put("codeEliminationMode", new EnumOption(CodeEliminationMode.PropagateConstants));
+		options.put("computeResultPaths", new BooleanOption(true));
+		options.put("enableArraySizeTainting", new BooleanOption(true));
+		options.put("enableCallbacks", new BooleanOption(true));
+		options.put("enableCallbackSources", new BooleanOption(true));
+		options.put("enableExceptions", new BooleanOption(true));
+		options.put("enableImplicitFlows", new BooleanOption(false));
+		options.put("enableIncrementalReporting", new BooleanOption(false));
+		options.put("enableStaticTracking", new BooleanOption(true));
+		options.put("enableTaintAnalysis", new BooleanOption(true));
+		options.put("enableTypeChecking", new BooleanOption(true));
+		options.put("flowSensitiveAliasing", new BooleanOption(true));
+		options.put("ignoreFlowsInSystemPackages", new BooleanOption(true));
+		options.put("inspectSinks", new BooleanOption(false));
+		options.put("inspectSources", new BooleanOption(false));
+		options.put("layoutMatchingMode", new EnumOption(LayoutMatchingMode.MatchSensitiveOnly));
+		options.put("logSourcesAndSinks", new BooleanOption(false));
+		options.put("maxThreadNum", new IntOption(-1));
+		options.put("mergeNeighbors", new BooleanOption(false));
+		options.put("oneResultPerAccessPath", new BooleanOption(false));
+		options.put("pathAgnosticResults", new BooleanOption(true));
+		options.put("pathBuilder", new EnumOption(PathBuilder.ContextInsensitiveSourceFinder));
+		options.put("stopAfterFirstKFlows", new IntOption(0));
+		options.put("useRecursiveAccessPaths", new BooleanOption(true));
+		options.put("useThisChainReduction", new BooleanOption(true));
+		options.put("useTypeTightening", new BooleanOption(true));
+		options.put("writeOutputFiles", new BooleanOption(false));
+		
 		return options;
 	}
 
@@ -154,21 +175,41 @@ public class FlowDroidFactory {
 		} else {
 			app = new SetupApplication(platforms, apk, im);
 		}
-
-		// @formatter:off
-		app.setStopAfterFirstFlow((boolean) options.get("stopAfterFirstFlow").getValue());
-		app.setEnableImplicitFlows((boolean) options.get("implicitFlows").getValue());
-		app.setEnableStaticFieldTracking((boolean) options.get("staticTracking").getValue());
-		app.setEnableCallbacks((boolean) options.get("enableCallbacks").getValue());
-		app.setEnableExceptionTracking((boolean) options.get("enableExceptions").getValue());
-		app.setFlowSensitiveAliasing((boolean) options.get("flowSensitiveAliasing").getValue());
-		app.setComputeResultPaths((boolean) options.get("computeResultPaths").getValue());
-		app.setAccessPathLength((int) options.get("accessPathLength").getValue());
-		app.setLayoutMatchingMode((LayoutMatchingMode) options.get("layoutMatchingMode").getValue());
-		app.setPathBuilder((PathBuilder) options.get("pathBuilder").getValue());
-		app.setCallgraphAlgorithm((CallgraphAlgorithm) options.get("callgraphAlgorithm").getValue());
+		InfoflowAndroidConfiguration config = new InfoflowAndroidConfiguration();
+		InfoflowConfiguration.setAccessPathLength((int) options.get("accessPathLength").getValue());
+		config.setAliasingAlgorithm((AliasingAlgorithm) options.get("aliasingAlgorithm").getValue());
+		config.setCallbackAnalyzer((CallbackAnalyzer) options.get("callbackAnalyzer").getValue());
+		config.setCallgraphAlgorithm((CallgraphAlgorithm) options.get("callgraphAlgorithm").getValue());
+		config.setCodeEliminationMode((CodeEliminationMode) options.get("codeEliminationMode").getValue());
+		config.setComputeResultPaths((boolean) options.get("computeResultPaths").getValue());
+		config.setEnableArraySizeTainting((boolean) options.get("enableArraySizeTainting").getValue());
+		config.setEnableCallbacks((boolean) options.get("enableCallbacks").getValue());
+		config.setEnableCallbackSources((boolean) options.get("enableCallbackSources").getValue());
+		config.setEnableExceptionTracking((boolean) options.get("enableExceptions").getValue());
+		config.setEnableImplicitFlows((boolean) options.get("enableImplicitFlows").getValue());
+		config.setIncrementalResultReporting((boolean) options.get("enableIncrementalReporting").getValue());
+		config.setEnableStaticFieldTracking((boolean) options.get("enableStaticTracking").getValue());
+		config.setTaintAnalysisEnabled((boolean) options.get("enableTaintAnalysis").getValue());
+		config.setEnableTypeChecking((boolean) options.get("enableTypeChecking").getValue());
+		config.setFlowSensitiveAliasing((boolean) options.get("flowSensitiveAliasing").getValue());
+		config.setIgnoreFlowsInSystemPackages((boolean) options.get("ignoreFlowsInSystemPackages").getValue());
+		config.setInspectSinks((boolean) options.get("inspectSinks").getValue());
+		config.setInspectSources((boolean) options.get("inspectSources").getValue());
+		config.setLayoutMatchingMode((LayoutMatchingMode) options.get("layoutMatchingMode").getValue());
+		config.setLogSourcesAndSinks((boolean) options.get("logSourcesAndSinks").getValue());
+		config.setMaxThreadNum((int) options.get("maxTheadNum").getValue());
+		InfoflowConfiguration.setMergeNeighbors((boolean) options.get("mergeNeighbors").getValue());
+		InfoflowConfiguration.setOneResultPerAccessPath((boolean) options.get("oneResultPerAccessPath").getValue());
+		InfoflowConfiguration.setPathAgnosticResults((boolean) options.get("pathAgnosticResults").getValue());
+		config.setPathBuilder((PathBuilder) options.get("pathBuilder").getValue());
+		config.setStopAfterFirstKFlows((int) options.get("stopAfterFirstKFlows").getValue());
+		InfoflowConfiguration.setUseRecursiveAccessPaths((boolean) options.get("useRecursiveAccessPaths").getValue());
+		InfoflowConfiguration.setUseThisChainReduction((boolean) options.get("useThisChainReduction").getValue());
+		InfoflowConfiguration.setUseTypeTightening((boolean) options.get("useTypeTightening").getValue());
+		config.setWriteOutputFiles((boolean) options.get("writeOutputFiles").getValue());
+		
+		app.setConfig(config);
 		app.calculateSourcesSinksEntrypoints((String) options.get("sourcesAndSinksFile").getValue());
-		// @formatter:on
 
 		String taintWrapper = (String) options.get("taintWrapperFile")
 				.getValue();
